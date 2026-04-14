41a41622a6c7c5366b53926b2a83f85c4fd236a1
Rubens Guilherme <78000574+rub1m@users.noreply.github.com>
2026-04-13 22:18:46 -0300

    up
    This program initializes three arrays representing coins in different rooms and uses pointers to print the values in each room.

---
+++ atv10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <locale.h>
+int main() { 
+  setlocale(LC_ALL,"Portuguese");
+    int sala_1[] = {10, 20, 5};
+    int sala_2[] = {50, 100};
+    int sala_3[] = {5, 5, 5, 10};
+   
+  int *ponteiro_salas[3];
+    ponteiro_salas[0] = sala_1;
+    ponteiro_salas[1] = sala_2;
+    ponteiro_salas[2] = sala_3;
+ 
+    printf("Moedas na Sala 1: ");
+    int tam1 = sizeof(sala_1) / sizeof(sala_1[0]); 
+    for (int i = 0; i < tam1; i++) {
+        printf("%d ", *(ponteiro_salas[0] + i));
+    }
+    printf("\n");
+    printf("Moedas na Sala 2: ");
+    int tam2 = sizeof(sala_2) / sizeof(sala_2[0]);
+    for (int i = 0; i < tam2; i++) {
+        printf("%d ", *(ponteiro_salas[1] + i));
+    }
+    printf("\n");
+    printf("Moedas na Sala 3: ");
+    int tam3 = sizeof(sala_3) / sizeof(sala_3[0]);
+    for (int i = 0; i < tam3; i++) {
+        printf("%d ", *(ponteiro_salas[2] + i));
+    }
+    printf("\n");
+    return 0;
+}
