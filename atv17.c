41f78c38adecb0978d1d12b9e3b5e4342483bf8c
Rubens Guilherme <78000574+rub1m@users.noreply.github.com>
2026-04-13 22:58:55 -0300

    up
    Added a 2D array to represent the game map and implemented a loop to find spikes.

--- atv17.c
+++ atv17.c
@@ -10,5 +10,19 @@ int main() {
     printf("Pontuação antes do power-up: %d\n", pontuacao_jogador);
     aplicar_powerup_pontuacao_dupla(&pontuacao_jogador);
     printf("Pontuação apos o power-up: %d\n", pontuacao_jogador);
+    int mapa_fase1[5][8] = {
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 1, 0, 0, 2, 0, 1, 1},
+        {0, 0, 1, 1, 0, 1, 0, 0},
+        {1, 1, 1, 1, 2, 2, 1, 1}
+    };
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (mapa_fase1[i][j] == 2) {
+                printf("Espinho encontrado na linha %d, coluna %d\n", i, j);
+            }
+        }
+    }
     return 0;
 }
